f1f8b5dfeeddfd6c02551acad74fc194fe91f447
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-27 10:41:41 +0700

    tentukkan class

---
+++ NoOverriding/NoOverriding.cpp
@@ -0,0 +1,11 @@
+// NoOverriding.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class baseclass {
+
+};
+
+
